b39c4f8917913ba40d4981ada943dd99f0bca625
cs-fedy <fedi.abd001@gmail.com>
2023-04-01 14:16:58 +0100

    new notification record creation done

--- api/src/modules/notifications/new-quiz-notification.jobs.ts
+++ api/src/modules/notifications/new-quiz-notification.jobs.ts
@@ -1,7 +1,9 @@
 import { Process, Processor } from '@nestjs/bull'
 import { Inject } from '@nestjs/common'
 import { Job } from 'bull'
-import { Jobs, Queues, Repos } from 'src/utils/constants'
+import { Jobs, Models, NotificationStatus, Queues, Repos } from 'src/utils/constants'
+import generateId from 'src/utils/generate-id'
+import IQuizRepo from '../quizzes/i-quizzes.repository'
 import { NewQuizNotificationArgs } from './i-new-quiz-notification.events'
 import INewQuizNotificationRepo from './i-new-quiz-notification.repository'
 
@@ -9,10 +11,21 @@ import INewQuizNotificationRepo from './i-new-quiz-notification.repository'
 export class NewQuizNotificationConsumer {
   constructor(
     @Inject(Repos.newQuizNotification) private newQuizNotificationRepo: INewQuizNotificationRepo,
+    @Inject(Repos.quiz) private quizRepo: IQuizRepo,
   ) {}
 
   @Process(Jobs.newQuizNotification)
   async newQuizNotification({ data }: Job<NewQuizNotificationArgs>) {
-    // TODO: to implement
+    const existingQuiz = await this.quizRepo.getQuizById(data.quizId)
+    if (!existingQuiz) return
+
+    await this.newQuizNotificationRepo.saveNewQuizNotification({
+      ...data,
+      emitter: data.userId,
+      emittedAt: new Date(),
+      notificationId: generateId(),
+      status: NotificationStatus.unread,
+      type: Models.newQuizzes,
+    })
   }
 }
